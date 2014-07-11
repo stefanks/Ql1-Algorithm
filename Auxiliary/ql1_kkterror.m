bc0158a8aef34e8065513db397a182a4705a363f
stepa <solntsev@gmail.com>
2014-07-11 16:38:16 -0500

    Remove commented stuff

--- Auxiliary/ql1_kkterror.m
+++ Auxiliary/ql1_kkterror.m
@@ -21,28 +21,6 @@ for i = 1: size(x,1)
     
     finalerror = max(finalerror, verror);
 end
-% 
-% finalerror2 = 0;
-% 
-% for i = 1: size(x,1)
-%     u=max(x(i),0);
-%     v=max(-x(i),0);
-%     if g(i)>tau(i)
-%         finalerror2 = max(finalerror2, (g(i)+tau(i))*(u/(u+1)));
-%         finalerror2 = max(finalerror2, g(i)-tau(i));
-%     elseif g(i)<-tau(i)
-%         finalerror2 = max(finalerror2, -g(i)-tau(i));
-%         finalerror2 = max(finalerror2,(-g(i)+tau(i))*(v/(v+1)));  
-%     else            
-%         finalerror2 = max(finalerror2, (g(i)+tau(i))*(u/(u+1)));
-%         finalerror2 = max(finalerror2,(-g(i)+tau(i))*(v/(v+1))); 
-%     end
-% end
-% 
-% if norm(finalerror2-finalerror)>1e-16
-%     fprintf('ERORRRRRRRRRR');
-%     finalerror= 'error';
-% end
 
 end
 
