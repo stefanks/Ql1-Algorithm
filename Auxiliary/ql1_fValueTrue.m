0733036748a0aa47cf8e7f14c72ef488acc95886
stepa <solntsev@gmail.com>
2014-07-11 16:38:43 -0500

    Deleted unused

--- Auxiliary/ql1_fValueTrue.m
+++
@@ -1,3 +0,0 @@
-function ret = ql1_fValueTrue(Ax,b,tau,x)
-ret = 1/2 * x' * Ax(x) - b' * x + sum(tau.*abs(x));
-end
--- Auxiliary/ql1_istastep.m
+++
@@ -1,4 +0,0 @@
-function step = ql1_istastep(g,tau,x,alphabar)
-step= max(x-alphabar*(g+tau),0) - max(-x-alphabar*(-g+tau),0)-x;            
-end
-
